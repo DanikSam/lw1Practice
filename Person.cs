dac73100daa146f03bf535fafbf928b7811dd9ad
DanikSam <samokaev_00@mail.ru>
2023-03-04 10:19:21 +0300

    Initial commit

---
+++ Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lw1Practice
+{
+    //internal - виден внутри сборки
+    internal class Person
+    {
+        //private - виден только внутри класса
+        private string _name;
+        //свойство Name
+        public string Name
+        {
+            get
+            {
+                return _name;   
+            }
+
+            set
+            {
+                //value - передаваемый параметр
+                _name = value;  
+            }
+        }
+        public Person(string name)
+        {
+            _name = name;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace lw1Practice
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            /*  Console.WriteLine("Hello, World!");
+              var name = Console.ReadLine();
+              Console.WriteLine(name);
+            */
+            Person person = new Person("Man");
+            person.Name = "Daniil";
+            Console.WriteLine(person.Name); 
+        }
+    }
+}
